f12f116d3af2365b866a4c3425f010196200840b
fraserdale <fraserdale2@gmail.com>
2020-01-27 13:56:00 +0000

    chapter 3 completed URL mapping and end exercises for about page

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+	return HttpResponse("<a href='/rango/about'>About</a> <br> Rango says hey there partner!")
+
+def about(request):
+	return HttpResponse("<a href='/rango/'>Home</a> <br> Rango says here is the about page.")
