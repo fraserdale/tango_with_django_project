d6ea0cd60aeed5ac9a9bc2a7134f9b3f7ab56c49
fraserdale <fraserdale2@gmail.com>
2020-01-31 17:02:24 +0000

    ochapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -24,14 +24,14 @@ def populate():
 		'url':'http://flask.pocoo.org'} 
 	]
 
-	cats = {'Python': {'pages': python_pages},
-		'Django': {'pages': django_pages},
-		'Other Frameworks': {'pages': other_pages} }
+	cats = {'Python': {'pages': python_pages, 'views':128, 'likes':64},
+        'Django': {'pages': django_pages, 'views':64, 'likes':32},
+        'Other Frameworks': {'pages': other_pages, 'views':32, 'likes':16} }
 
 	# If you want to add more categories or pages, # add them to the dictionaries above.
 	# The code below goes through the cats dictionary, then adds each category, # and then adds all the associated pages for that category.
 	for cat, cat_data in cats.items():
-		c = add_cat(cat)
+		c = add_cat(cat, cat_data['views'], cat_data['likes'])
 		for p in cat_data['pages']:
 			add_page(c, p['title'], p['url'])
 			# Print out the categories we have added.
@@ -45,12 +45,16 @@ def add_page(cat,title,url,views=0):
 	p.views=views
 	p.save()
 	return p
-def add_cat(name):
-	c = Category.objects.get_or_create(name=name)[0] 
-	c.save()
-	return c
+
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
+    c.save()
+    return c
 
 #Startexecutionhere!
 if __name__=='__main__':
 	print('Starting Rango population script...') 
 	populate()
+	
--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+from django.contrib import admin
+from rango.models import Category, Page
+
+# Add in this class to customise the Admin Interface 
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug':('name',)}
+	# Update the registration to include this customised interface
+
+admin.site.register(Category, CategoryAdmin)
 
 # Register your models here.
 from rango.models import Category, Page
-admin.site.register(Category) 
-admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','category','url')
+
+admin.site.register(Page, PageAdmin)
+
